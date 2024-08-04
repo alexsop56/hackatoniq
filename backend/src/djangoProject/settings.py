50ee178a3cd917048bb24ebc0b8e3736c6ade709
alexsop <alexsandr246ilek@gmail.com>
2024-08-04 11:32:47 +0300

    fx cors

--- backend/src/djangoProject/settings.py
+++ backend/src/djangoProject/settings.py
@@ -45,6 +45,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -65,6 +66,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
